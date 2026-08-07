24e1ee52011cc31d4589887c0dd4e25114dd179f
sonali-khambale <khambalesonali@gmail.com>
2026-08-07 22:10:01 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -20,10 +20,9 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(
-            error_message, error_detail=error_detail
+            error_message, error_detail
         )
 
     def __str__(self):
         return self.error_message
-
 
