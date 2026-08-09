2aac36495ed98b78819cf029e51e3be110829b76
sonali-khambale <khambalesonali@gmail.com>
2026-08-09 23:04:04 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -26,6 +27,7 @@ class DataIngestion:
 
         try:
             df = pd.read_csv(r"notebook\data\stud.csv")
+
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(
@@ -71,5 +73,14 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
+
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    data_transformation.initiate_data_transformation(
+        train_data,
+        test_data
+    )
